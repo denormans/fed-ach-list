3ed28b184818bb4d599fd2872496accb3a8d636c
Michael Norman <michael.norman@denormans.com>
2016-01-18 22:25:18 -0600

    Added S3 support

--- services/io/achS3.js
+++ services/io/achS3.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var aws = require('aws-sdk');
+var Q = require('q');
+
+var s3 = new aws.S3();
+
 var config = require('../../utils/config.js');
 
 /**
@@ -8,7 +13,12 @@ var config = require('../../utils/config.js');
  * @return {Promise<array<AchInfo>>} ACH list
  */
 function load() {
-  throw Error('S3 unsupported');
+  return Q.nbind(s3.getObject, s3)({
+    Bucket: config.achS3Bucket,
+    Key: config.achS3Path
+  }).then(function (data) {
+    return JSON.parse(data.Body);
+  });
 }
 
 /**
@@ -17,7 +27,12 @@ function load() {
  * @param {Promise<array<AchInfo>>} achList ACH list
  */
 function save(achList) {
-  throw Error('S3 unsupported');
+  return Q.nbind(s3.putObject, s3)({
+    Bucket: config.achS3Bucket,
+    Key: config.achS3Path,
+    ContentType: 'application/json',
+    Body: JSON.stringify(achList)
+  });
 }
 
 module.exports = {
