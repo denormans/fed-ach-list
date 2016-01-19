518093d666cbfff171ec872366ce3b212d549b29
Michael Norman <michael.norman@denormans.com>
2016-01-18 21:51:08 -0600

    Better downloader of ACH file

--- downloader.js
+++ downloader.js
@@ -2,6 +2,7 @@
 
 var fedach = require('fedach');
 var fs = require('fs');
+var request = require('request');
 var Q = require('q');
 
 var achIO = require('./services/io/ach.js');
@@ -23,13 +24,44 @@ function downloadAndSave() {
  * @return {Promise<Array<AchInfo>>} ACH list
  */
 function download() {
+  var dataPromise;
+
   if (config.testAchFile) {
-    return Q.nbind(fs.readFile, fs)(config.testAchFile, 'utf8').then(function(data) {
-      return Q(fedach.parse(data));
-    })
+    dataPromise = Q.nbind(fs.readFile, fs)(config.testAchFile, 'utf8')
   } else {
-    return Q.nbind(fedach.download, fedach)();
+    var jar = request.jar();
+
+    dataPromise = Q.nfbind(request)({
+      uri: 'https://www.frbservices.org/EPaymentsDirectory/submitAgreement',
+      method: 'POST',
+      timeout: 10000,
+      followRedirect: true,
+      maxRedirects: 10,
+      jar: jar,
+      form: {
+        agreementValue: 'Agree'
+      }
+    }).then(function(response) {
+      return Q.nfbind(request)({
+        uri: 'https://www.frbservices.org/EPaymentsDirectory/FedACHdir.txt',
+        method: 'GET',
+        timeout: 10000,
+        followRedirect: true,
+        maxRedirects: 10,
+        jar: jar
+      });
+    }).then(function(response) {
+      if (response.length < 2) {
+        throw Error('No response body from frbservices.org');
+      }
+
+      return response[1];
+    });
   }
+
+  return dataPromise.then(function(data) {
+    return fedach.parse(data);
+  })
 }
 
 module.exports = {
